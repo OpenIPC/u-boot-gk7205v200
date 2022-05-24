baeb2caad6fb670a21d2c05d7b9a0dcd5262112b
Dmitry Ermakov <de@dimer.org.ua>
2022-05-24 23:30:18 +0300

    Add BY25Q64AS

--- drivers/mtd/spi/fmc100/fmc_spi_nor_ids.c
+++ drivers/mtd/spi/fmc100/fmc_spi_nor_ids.c
@@ -1852,6 +1852,29 @@ static struct spi_nor_info fmc_spi_nor_info_table[] = {
 		&spi_driver_general,
 	},
 
+	{
+		"BY25Q64AS", {0x68, 0x40, 0x17}, 3, _8M,  _64K, 3,
+		{
+			&read_std(0, INFINITE, 45),
+			&read_fast(1, INFINITE, 104),
+			&read_dual(1, INFINITE, 104),
+			&read_dual_addr(1, INFINITE, 104),
+			&read_quad(1, INFINITE, 104),
+			&read_quad_addr(3, INFINITE, 104),
+			0
+		},
+		{
+			&write_std(0, 256, 104),
+			&write_quad(0, 256, 104),
+			0
+		},
+		{
+			&erase_sector_64k(0, _64K, 104),
+			0
+		},
+		&spi_driver_general,
+	},
+
 	{0, {0}, 0, 0, 0, 0, {0}, {0}, {0}, NULL},
 };
 
