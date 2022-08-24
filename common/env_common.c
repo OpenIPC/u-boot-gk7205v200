662803639afa22d46bee64969874a4a4cdafdb2f
Dmitry Ermakov <de@dimer.org.ua>
2022-08-24 21:28:52 +0300

    Overwrite env only if first 8 symbols @ 0x40000 are 0xFF

--- common/env_common.c
+++ common/env_common.c
@@ -201,6 +201,10 @@ int env_import(const char *buf, int check)
 
 		if (crc32(0, ep->data, ENV_SIZE) != crc) {
 			set_default_env("!bad CRC");
+			if (crc == 0xffffffff) {
+				printf("Saving default environment...\n");
+				saveenv();
+			}
 			return 0;
 		}
 	}
